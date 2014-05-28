818d9d1924d1374e24d026eb2a2bc50912a28f1e
K-4U <koen@k-4u.nl>
2014-05-28 17:40:13 +0200

    First test

--- src/k4unl/minecraft/colorchat/events/EventHelper.java
+++ src/k4unl/minecraft/colorchat/events/EventHelper.java
@@ -1,11 +1,20 @@
 package k4unl.minecraft.colorchat.events;
 
+import k4unl.minecraft.colorchat.lib.Log;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ServerChatEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class EventHelper {
 
 	
 	public static void init(){
 		MinecraftForge.EVENT_BUS.register(new EventHelper());
+	}
+	
+	@SubscribeEvent
+	public static void chatEvent(ServerChatEvent event){
+		Log.info("We got a chat message: " + event.username + ":" + event.message);
+		event.setCanceled(true);
 	}
 }
